7302459e4a5f30831effefa5dcb23244f74dd7c5
SnapRock <marcio.dias@preemptivatecnologia.com.br>
2024-10-09 10:32:51 -0300

    V. Inicial

--- app_nota_certa.py
+++ app_nota_certa.py
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLay
 class NcmApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Obter NCM")
+        self.setWindowTitle("Obter NCM V.1.0")
         self.resize(500, 400)
 
         # Widgets
